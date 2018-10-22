98b8f6e7ab8aef8fcd9b9e130f040559a7fa5700
Veronica Bustabad <vbustabad@gmail.com>
2018-10-22 19:07:35 +0000

    automatically backed up by learn

--- src/createStore.js
+++ src/createStore.js
@@ -1,15 +1,17 @@
 export default function createStore(reducer) {
   let state;
- 
+
   function dispatch(action) {
     state = reducer(state, action);
     render();
   }
- 
+
   function getState() {
     return state;
   };
- 
+
+  dispatch({type: '@@INIT'})
+
   return {
     dispatch,
     getState
